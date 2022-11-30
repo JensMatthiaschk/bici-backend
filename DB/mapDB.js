bbb628c2497794b80ff2aef8dbf4d3f4a60db2bb
fabian-elmar <fabsiban09@gmail.com>
2022-11-30 09:17:09 +0100

    data massage

--- DB/mapDB.js
+++ DB/mapDB.js
@@ -1,16 +1,32 @@
 import mongoose from "mongoose"
 
+const pointSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        enum: ['Point'],
+        required: true
+    },
+    coordinates: {
+        type: [Number],
+        required: true
+    }
+});
 
 const MapSchema = new mongoose.Schema(
     {
         user: {
             required: true,
             type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
+            ref: "user",
+            path: ""
         },
+
         camping: { type: Boolean, default: false },
         description: { type: String },
-        location: { type: String },
+        location: {
+            type: pointSchema,
+            required: true
+        },
         events: { type: Boolean, default: false },
         host: { type: Boolean, default: false },
         reapir: { type: Boolean, default: false },
--- controllers/map.js
+++ controllers/map.js
@@ -1,11 +1,20 @@
 import SetPin from "../DB/mapDB";
 import * as jwt from "../utilities/jwt.js"
 import mongoose from "mongoose"
+import { transform } from "./utils";
+
+
+
+
+
 
 
 export const editMapPin = async (req, res) => {
+
+    console.log('location', req.body.location)
+
+
     console.log('PINreq', req.token)
-    console.log('pinbody', req.body)
     if (!req.token.id) {
         return res.status(404).send({
             message: "Map >You're not logged in ",
@@ -14,23 +23,38 @@ export const editMapPin = async (req, res) => {
         })
     }
     try {
+        console.log('pinbody', req.body)
+        console.log('just transform', parseFloat(transform(req.body.location)[1]))
+        const userId = mongoose.Types.ObjectId(req.token.id);
+        const coordiantes = req.body.location.slice(6)
+        console.log(coordiantes)
+        let foundPin = await SetPin.findOne({ location: [parseFloat(transform(req.body.location)[0]), parseFloat(transform(req.body.location)[1])] })
+        console.log('founs?', foundPin)
+        if (!foundPin) {
+            await SetPin.create({
+                user: userId,
+                camping: req.body.camping,
+                description: req.body.description,
+                location: [parseFloat(transform(req.body.location)[0]), parseFloat(transform(req.body.location)[1])],
+                events: req.body.events,
+                host: req.body.events,
+                reapir: req.body.events,
+                shower: req.body.events,
+                swim: req.body.events,
+            })
 
-        //const userId = mongoose.Types.ObjectId();
-        //let foundPin = await SetPin.findOne({ id: req.token.id })
-        //   if (!foundProfile) {
-        await SetPin.create(req.body, { user: req.token.id })
-            /  /*  } else {
-            foundProfile.update(, { new: true }, (err, data) => {
+        } else {
+            foundPin.updateOne(req.body, { new: true }, (err, data) => {
                 if (err) return console.log(err)
                 console.log(data)
             })
-        } */
+        }
 
-            res.send({
-                message: "Map > Pin successfully set",
-                data: foundPin,
-                success: true,
-            })
+        res.send({
+            message: "Map > Pin successfully set",
+            data: foundPin,
+            success: true,
+        })
 
     } catch (error) {
         res.status(400).send({
